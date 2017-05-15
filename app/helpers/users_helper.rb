2855b013560e6d848292f39446c484622288386e
Wen-Tien Chang <ihower@gmail.com>
2017-05-15 15:51:39 +0800

    Fix XSS

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,9 +7,10 @@ module UsersHelper
     email_md5 = Digest::MD5.hexdigest(user.email)
     gravatar_url = "https://www.gravatar.com/avatar/#{email_md5}"
 
-    str = "<div class ='user-link'>" + link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name + "</div>"
-
-    str.html_safe
+    # "<div class ='user-link'>".html_safe + link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name + "</div>".html_safe
+    content_tag(:div,
+      link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name,
+      :class => "user-link" )
   end
 
 end
