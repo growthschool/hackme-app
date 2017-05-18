7832ec035b02e91e495bf76accc7916f0090416d
cndaniel <cndaniel@me.com>
2017-05-18 17:52:39 +0200

    use content_tag

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,9 +7,11 @@ module UsersHelper
     email_md5 = Digest::MD5.hexdigest(user.email)
     gravatar_url = "https://www.gravatar.com/avatar/#{email_md5}"
 
-    "<div class ='user-link'>".html_safe + link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name + "</div>".html_safe
+    # "<div class ='user-link'>".html_safe + link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name + "</div>".html_safe
 
-
+    content_tag(:div,
+    link_to(image_tag(gravatar_url), user_path(user)) + " " + user.display_name ,
+    :class => "user-link" )
   end
 
 end
