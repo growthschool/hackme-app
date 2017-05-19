b29b40f2f0f22c6f54f95b02394d504c4b4d0a7a
zhangyan <tongkusat123@gmail.com>
2017-05-19 19:49:07 +0800

    security before cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,25 @@
 class UsersController < ApplicationController
-
-  def show
-    @user = User.find( params[:id] )
-  end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-
-    if @user.update(user_params)
-      redirect_to user_path(@user)
-    else
-      render "edit"
+    def show
+        @user = User.find(params[:id])
     end
-  end
 
-  protected
+    def edit
+        @user = current_user
+    end
 
-  def user_params
-    params.require(:user).permit(:nickname, :role)
-  end
+    def update
+        @user = current_user
 
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+        end
+    end
+
+    protected
+
+    def user_params
+        params.require(:user).permit(:nickname)
+    end
 end
