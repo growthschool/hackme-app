82641b84a5d36f145246d6797ce10bc88a4536f7
Antarts <m.dream@foxmail.com>
2017-06-27 23:19:19 +0800

    fix sort by DESC & ASC to attack SQL

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,7 @@ class EventsController < ApplicationController
       @comments = @comments.where( "comments.content LIKE ?", "%#{params[:keyword]}%")
     end
 
-    if params[:sort]
+    if params[:sort] && ["id DESC", "id ASC"].include?(params[:sort])   # 只有白名单内的参数可以用
       @comments = @comments.order(params[:sort])
     end
 
