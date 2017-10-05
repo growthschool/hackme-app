a8fd431ad642f0eb9e23c4475d6c90eea82eb8b2
Your Name <you@example.com>
2017-10-05 10:49:53 +0800

    SQL Injection 数据库注入攻击，漏网之鱼

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
 
     end
 
-    if params[:sort]
+    if params[:sort] && ["id DESC", "id ASC"].include?(params[:sort])  # 只有白名单内的参数可以用
       @comments = @comments.order(params[:sort])
     end
 
